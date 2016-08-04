415afb588ddfb6a03a3e82ac4bb10f544644ca02
Chinh Tran <ducchinhbk@gmail.com>
2016-08-04 07:11:00 +0700

    delete all

--- admincp/wp-content/themes/enang/searchform.php
+++
@@ -1,4 +0,0 @@
-<form role="search" class="frm-search" id="search-frm" method="GET" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<input name="s" value="<?php echo esc_attr( get_search_query() ); ?>" class="txt-search" type="text" placeholder="Nội dung cần tìm..">
-	<input class="btn-search" type="submit" value="" style="background-color: #f9f9f9;">
-</form>
--- admincp/wp-content/themes/vnup/searchform.php
+++
@@ -1,4 +0,0 @@
-<form role="search"  method="GET" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-    <input class="input-search" type="text" name="s" id="txtSearchTin" placeholder="TÌM KIẾM" />
-    <input class="sprite btnsearch" type="submit" value=""/>
-</form>
